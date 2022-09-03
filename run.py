46309325ce709617519b35d07d8efe41a5cf595b
MuttiD <dgmutti@hotmail.com>
2022-09-03 18:18:31 +0000

    Add validation to scrap data

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+#from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,27 +13,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('bolts_control')
 
-def get_wip_user():
+def get_wip_scrap_user():
     """
-    Get the work-in-progress data of super bolts from user
+    Get the work-in-progress data and srapped bolts from user
     Add a while loop to collect a valid string of data from the user
     via the ternminal, that must be a string of 6 integer numbers separated
     by commas. The loop will keep repeating til it gets the correct set of numbers
     """
     while True:   
-        print("Please provide WIP data from today's work: ")
+        print("Please provide WIP and SCRAP data from today's work. ")
         print("Data should be six numbers, separated by commas.")
+        print("In the SCRAP data, please be aware that: ")
+        print("   - Positive scrap means bolts refused")
+        print("   - Negative scrap means bolts recovered")
         print("Example: 21,23,10,54,20,36")
 
         data_str = input("Enter your WIP here: ")
         wip_data = data_str.split(",")
-        
-        if validate_data(wip_data):
-            print("This data is valid! Thank you")
 
-            break 
+        scrap_str = input("Enter the scrapped data here: ")
+        scr_data = scrap_str.split(",")
+        
+        
+        if validate_data(wip_data) and validate_data(scr_data):
+            print("Data provided is valid! Thank you")
+            break
 
     return wip_data
+    return scr_data
 
 def validate_data(values):
     """
@@ -41,7 +48,7 @@ def validate_data(values):
     Raising ValueError if strings cannot be converted into it,
     or if there aren't exactly six values
     """
-    print(values)
+    #print(values)
     try:
         [int(value) for value in values]            # this will convert any string in values' list
         if len(values) != 6:
@@ -64,11 +71,21 @@ def update_worksheet(wip, worksheet):
     worksheet_to_update.append_row(wip)
     print(f"{worksheet} worksheet updated successfully.\n")
 
+def update_scrap_worksheet(scrap, worksheet):
+    """
+    This function will receives a list of integers from (scrap)
+    And it will update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(scrap)
+    print(f"{worksheet} worksheet updated successfully.\n")
+
 
 def calculate_inventory_data(wip_row):
     """
     The inventory is defined as the work-in-progress less the scrapped bolts.
-    - positive scrap are bolts being left to recover
+    - positive scrap are bolts refused
     - negative scrap are bolts recovered
     """
     print("Calculating inventory....\n")
@@ -83,16 +100,27 @@ def calculate_inventory_data(wip_row):
     
     return inventory_data
 
+    #for inventory_data < 1:
+    #     print(f"   *** Warning: {Column} is out of stock. Get more of these for tomorrow.")
+
 
 def main():
     """
     Running all program functions
     """
-    wip = get_wip_user()
+    wip = get_wip_scrap_user()
+    
     wip_data = [int(num) for num in wip]
+    scrap_data = [int(i) for i in wip]
+
     update_worksheet(wip_data, "WIP")
+    update_scrap_worksheet(scrap_data, "scrap")
+
     new_inventory_data = calculate_inventory_data(wip_data)
     update_worksheet(new_inventory_data, "inventory")
+
+    
+
     
 
 
