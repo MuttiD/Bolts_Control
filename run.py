a43f358f7fd1678e82016515aed28c90148bd8b5
MuttiD <dgmutti@hotmail.com>
2022-09-04 11:02:20 +0000

    Passed PEP8 standard

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-#from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,39 +12,39 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('bolts_control')
 
+
 def get_wip_scrap_user():
     """
     Get the work-in-progress data and srapped bolts from user
     Add a while loop to collect a valid string of data from the user
     via the ternminal, that must be a string of 6 integer numbers separated
-    by commas. The loop will keep repeating til it gets the correct set of numbers
+    by commas. The loop will keep repeating til it gets the
+    correct set of numbers
     """
-    while True:   
+    while True:
         print("Please provide WIP and SCRAP data from today's work. ")
         print("Data should be six numbers, separated by commas.")
         print("In the SCRAP data, please be aware that: ")
         print("   - Positive scrap means bolts refused")
-        print("   - Negative scrap means bolts recovered")
+        print("   - Negative scrap means bolts repaired/recovered")
         print("Example: 21,23,10,54,20,36")
 
         data_str = input("Enter your WIP here: ")
         wip_data = data_str.split(",")
-        
 
         scrap_str = input("Enter the scrapped data here: ")
         scr_data = scrap_str.split(",")
-        
-        
+
         if validate_data(wip_data) and validate_data(scr_data):
             print("Data provided is valid! Thank you")
-        
+
             wip_data = [int(w) for w in wip_data]
-            scr_data = [int(s) for s in scr_data]  
-            
+            scr_data = [int(s) for s in scr_data]
+
             break
 
-
     return (wip_data, scr_data)
+
 
 def validate_data(values):
     """
@@ -53,9 +52,9 @@ def validate_data(values):
     Raising ValueError if strings cannot be converted into it,
     or if there aren't exactly six values
     """
-    #print(values)
     try:
-        [int(value) for value in values]            # this will convert any string in values' list
+        # this will convert any string in values' list
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"It is required 6 values, you have provided {len(values)}"
@@ -63,9 +62,9 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
 
     return True
+
 
 def update_worksheet(values, worksheet):
     """
@@ -73,39 +72,28 @@ def update_worksheet(values, worksheet):
     And it will update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet...\n")
-    #print([type(x) for x in values])
-    
+
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(values)
     print(f"{worksheet} worksheet updated successfully.\n")
-
-# def update_scrap_worksheet(scrap, worksheet):
-#     """
-#     This function will receives a list of integers from (scrap)
-#     And it will update the relevant worksheet with the data provided
-#     """
-#     print(f"Updating {worksheet} worksheet...\n")
-#     worksheet_to_update = SHEET.worksheet(worksheet)
-#     worksheet_to_update.append_row(scrap)
-#     print(f"{worksheet} worksheet updated successfully.\n")
 
 
 def calculate_inventory_data(wip_row):
     """
     The inventory is defined as the work-in-progress less the scrapped bolts.
     - positive scrap are bolts refused
-    - negative scrap are bolts recovered
+    - negative scrap are bolts repaired/recovered
     """
     print("Calculating inventory....\n")
     scrap = SHEET.worksheet("scrap").get_all_values()
     scrap_row = scrap[-1]                       # -1 last row in the sheet
-    
+
     inventory_data = []
 
     for scrap, wip in zip(scrap_row, wip_row):
         inventory = wip - int(scrap)
         inventory_data.append(inventory)
-    
+
     return inventory_data
 
     #for inventory_data < 1:
@@ -117,7 +105,7 @@ def main():
     Running all program functions
     """
     wip = get_wip_scrap_user()
-    
+
     wip_data = wip[0]
     scrap_data = wip[1]
 
@@ -127,8 +115,6 @@ def main():
     new_inventory_data = calculate_inventory_data(wip_data)
     update_worksheet(new_inventory_data, "inventory")
 
-
 print("")
 print("       **** Welcome to the Bolts Control Programm **** \n")
 main()
-
